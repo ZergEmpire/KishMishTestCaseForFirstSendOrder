fe868d88cc879f0e5e814275afc0d056d66b3aad
ZergEmpire <71401820+ZergEmpire@users.noreply.github.com>
2021-11-02 10:52:10 +0300

    Initial commit
    Initial commit

---
+++ src/test/java/FirstTestCase/TestClass.java
@@ -0,0 +1,28 @@
+package FirstTestCase;
+
+import org.junit.Test;
+
+public class TestClass extends TestBase {
+
+
+
+
+
+
+    @Test
+    public void userCanSendOrder() {
+        openURL();
+        mathRandomHead();
+        pickRandCards();
+        goBasket();
+        selectDeliveryTypePickUp();
+        selectTerminalForPickUp();
+        fillInFields();
+
+
+
+
+    }
+
+
+}
